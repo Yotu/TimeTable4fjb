6f6d6466d16d08fc8111e996a36219f278cdae08
acer-aspire <acer-aspire@172.16.13.138>
2013-01-23 18:24:03 +0900

    xml修正

--- src/jp/sample/time_table/TimeTableEditActivity.java
+++ src/jp/sample/time_table/TimeTableEditActivity.java
@@ -175,175 +175,180 @@ public class TimeTableEditActivity extends Activity implements OnClickListener {
 	}
 
 	public void onClick(View v) {
-		Log.d(TAG, "TimeTableEditActivity onClick");
-		TimeTableInfo info = new TimeTableInfo();
-		info.setTitle(titleEdt.getText().toString());
-		info.setDayOfWeek(weekSpr.getSelectedItem().toString());
-		info.setTimeTable(timeTableSpr.getSelectedItem().toString());
-		info.setTodo(todoEdt.getText().toString());
-		info.setType(typeSpr.getSelectedItem().toString());
-		info.setPlace(placeEdt.getText().toString());
-		info.setIsShare(shareCb.isChecked());
-		info.setBikoShare(bikoShareCb.isChecked());
-		Log.d(TAG,"インスタンス、セッター");
+		if(v == findViewById(R.id.cancelButton)){
 
-		// 現在のUnixタイム取得
-		long currentTimeMillis = System.currentTimeMillis();
-		// 数値から文字列に変更
-		String timestamp = String.valueOf(currentTimeMillis);
-		Log.d(TAG,"タイム取得");
+		}else {
+			Log.d(TAG, "TimeTableEditActivity onClick");
 
-		ContentValues values = new ContentValues();
+			TimeTableInfo info = new TimeTableInfo();
+			info.setTitle(titleEdt.getText().toString());
+			info.setDayOfWeek(weekSpr.getSelectedItem().toString());
+			info.setTimeTable(timeTableSpr.getSelectedItem().toString());
+			info.setTodo(todoEdt.getText().toString());
+			info.setType(typeSpr.getSelectedItem().toString());
+			info.setPlace(placeEdt.getText().toString());
+			info.setIsShare(shareCb.isChecked());
+			info.setBikoShare(bikoShareCb.isChecked());
+			Log.d(TAG,"インスタンス、セッター");
 
-		//		-----------------------------------------------------------------------
-		//
-		//		チェック項目を取得&インデックスを取得
-		//
-		//		-----------------------------------------------------------------------
-		if (shareCb.isChecked()) {
-			isShare = 1; // 時間割シェアのチェック判定
-		}
-		if (bikoShareCb.isChecked()) {
-			bikoShare = 1; // 備考シェアのチェック判定
-		}
+			// 現在のUnixタイム取得
+			long currentTimeMillis = System.currentTimeMillis();
+			// 数値から文字列に変更
+			String timestamp = String.valueOf(currentTimeMillis);
+			Log.d(TAG,"タイム取得");
 
-		timeid = timeTableSpr.getSelectedItemPosition() + 1; //時限IDのインデックス
-		week = weekSpr.getSelectedItemPosition(); // 曜日のインデックス
-		typeid = typeSpr.getSelectedItemPosition(); // 種類のインデックス
-		Log.d(TAG,"チェック項目＆＆インデックス取得");
-		Log.d(TAG,"曜日番号を取得 ="+week+" 種類番号="+ typeid +"時限ID="+ timeid);
-		//		-----------------------------------------------------------------------
-		//
-		//		授業科目IDを取得
-		//
-		//		-----------------------------------------------------------------------
-		// 入力された情報を元に、関連のあるデータを検索　無ければ格納する
-		db = sqlHelper.getReadableDatabase();
-		String sql = "SELECT subjectid FROM subject WHERE subject_name = '"
-				+ info.getTitle() + "' AND place = '" + info.getPlace() + "';";
-		Log.d(TAG,"start query ="+sql);
-		Cursor c = db.rawQuery(sql, null);
-		Log.d(TAG,"start if");
-		if (c.getCount() == 0) {
-			// 検索結果ゼロ
-			Log.d(TAG, "検索結果ゼロ");
-			table = "subject";
-			ContentValues ct = new ContentValues();
-			ct.put("subject_name", info.getTitle());
-			ct.put("place", info.getPlace());
-			subjectid = (int)sqlHelper.insert(table, ct);
-			Log.d(TAG, "Subject追加完了");
-		}else{
-			c.moveToFirst();
-			subjectid = c.getInt(0);
-			Log.d(TAG,"既に登録されている科目データです。　index:"+subjectid);
-		}
-		c.close();
-		db.close();
+			ContentValues values = new ContentValues();
 
-		Log.d(TAG,"授業科目名取得");
-		//		-----------------------------------------------------------------------
-		//
-		//		ユーザーIDを取得
-		//
-		//		-----------------------------------------------------------------------
-		//自分のUIDを検索
-		//自分のUIDは必ず１になるので、1を指定する
-		creatorid = 1;
-
-		Log.d(TAG,"CreatorId取得");
-		//		-----------------------------------------------------------------------
-		//
-		//		備考情報の取得＆登録
-		//
-		//		-----------------------------------------------------------------------
-		if(info.getTodo() != null){
-			table = "remarks";
-
-			ContentValues ct = new ContentValues();
-			ct.put("date", timestamp);// ※ここをトップ画面から受け取った登録したい日時の情報にする
-			//========================================================================================================
-			//======================↑要注意箇所↑（現時点だと、下と同じtimestampが格納されるよこれ)==============================
-			//========================================================================================================
-			ct.put("timeid", timeid);
-			ct.put("remarks", info.getTodo());
-			ct.put("share", bikoShare);
-			ct.put("creatorid", creatorid);
-			ct.put("upremarks", timestamp);
-
-			db = sqlHelper.getReadableDatabase();
-			// timestampは、後でトップ画面から受け取った登録したい日時の情報に変更する
-			sql = "SELECT * FROM remarks WHERE date=" + timestamp + " and timeid=" + timeid + ";";
-			// このSQLで、～日の～時限目の備考情報があるかどうかをチェック
-			c = db.rawQuery(sql, null);
-			Log.v(TAG, String.format("c=%d件", c.getCount()));
-			if (c.getCount() == 0) {
-				// 検索結果ゼロの場合は新規登録
-				Log.d(TAG, "remarks insert");
-				sqlHelper.insert(table, ct);
-			} else {
-				// 検索結果がゼロでなかった場合は更新処理
-				// 脆弱性あり。実行確認優先
-				Log.d(TAG, "remarks update");
-				sqlHelper.update(table, ct, "date=" + timestamp + " and timeid=" + info.getTimeTable(), null);
+			//		-----------------------------------------------------------------------
+			//
+			//		チェック項目を取得&インデックスを取得
+			//
+			//		-----------------------------------------------------------------------
+			if (shareCb.isChecked()) {
+				isShare = 1; // 時間割シェアのチェック判定
 			}
+			if (bikoShareCb.isChecked()) {
+				bikoShare = 1; // 備考シェアのチェック判定
+			}
+
+			timeid = timeTableSpr.getSelectedItemPosition() + 1; //時限IDのインデックス
+			week = weekSpr.getSelectedItemPosition(); // 曜日のインデックス
+			typeid = typeSpr.getSelectedItemPosition(); // 種類のインデックス
+			Log.d(TAG,"チェック項目＆＆インデックス取得");
+			Log.d(TAG,"曜日番号を取得 ="+week+" 種類番号="+ typeid +"時限ID="+ timeid);
+			//		-----------------------------------------------------------------------
+			//
+			//		授業科目IDを取得
+			//
+			//		-----------------------------------------------------------------------
+			// 入力された情報を元に、関連のあるデータを検索　無ければ格納する
+			db = sqlHelper.getReadableDatabase();
+			String sql = "SELECT subjectid FROM subject WHERE subject_name = '"
+					+ info.getTitle() + "' AND place = '" + info.getPlace() + "';";
+			Log.d(TAG,"start query ="+sql);
+			Cursor c = db.rawQuery(sql, null);
+			Log.d(TAG,"start if");
+			if (c.getCount() == 0) {
+				// 検索結果ゼロ
+				Log.d(TAG, "検索結果ゼロ");
+				table = "subject";
+				ContentValues ct = new ContentValues();
+				ct.put("subject_name", info.getTitle());
+				ct.put("place", info.getPlace());
+				subjectid = (int)sqlHelper.insert(table, ct);
+				Log.d(TAG, "Subject追加完了");
+			}else{
+				c.moveToFirst();
+				subjectid = c.getInt(0);
+				Log.d(TAG,"既に登録されている科目データです。　index:"+subjectid);
+			}
+			c.close();
+			db.close();
+
+			Log.d(TAG,"授業科目名取得");
+			//		-----------------------------------------------------------------------
+			//
+			//		ユーザーIDを取得
+			//
+			//		-----------------------------------------------------------------------
+			//自分のUIDを検索
+			//自分のUIDは必ず１になるので、1を指定する
+			creatorid = 1;
+
+			Log.d(TAG,"CreatorId取得");
+			//		-----------------------------------------------------------------------
+			//
+			//		備考情報の取得＆登録
+			//
+			//		-----------------------------------------------------------------------
+			if(info.getTodo() != null){
+				table = "remarks";
+
+				ContentValues ct = new ContentValues();
+				ct.put("date", timestamp);// ※ここをトップ画面から受け取った登録したい日時の情報にする
+				//========================================================================================================
+				//======================↑要注意箇所↑（現時点だと、下と同じtimestampが格納されるよこれ)==============================
+				//========================================================================================================
+				ct.put("timeid", timeid);
+				ct.put("remarks", info.getTodo());
+				ct.put("share", bikoShare);
+				ct.put("creatorid", creatorid);
+				ct.put("upremarks", timestamp);
+
+				db = sqlHelper.getReadableDatabase();
+				// timestampは、後でトップ画面から受け取った登録したい日時の情報に変更する
+				sql = "SELECT * FROM remarks WHERE date=" + timestamp + " and timeid=" + timeid + ";";
+				// このSQLで、～日の～時限目の備考情報があるかどうかをチェック
+				c = db.rawQuery(sql, null);
+				Log.v(TAG, String.format("c=%d件", c.getCount()));
+				if (c.getCount() == 0) {
+					// 検索結果ゼロの場合は新規登録
+					Log.d(TAG, "remarks insert");
+					sqlHelper.insert(table, ct);
+				} else {
+					// 検索結果がゼロでなかった場合は更新処理
+					// 脆弱性あり。実行確認優先
+					Log.d(TAG, "remarks update");
+					sqlHelper.update(table, ct, "date=" + timestamp + " and timeid=" + info.getTimeTable(), null);
+				}
+				db.close();
+			}
+			Log.d(TAG,"備考取得");
+			//		-----------------------------------------------------------------------
+			//
+			//		===================timeデータベースにデータを保存===========================
+			//
+			//		-----------------------------------------------------------------------
+
+			//time  |曜日	|時限ID	|授業科目ID	|種類ID	|シェア（時間割)	|作成者ID		|更新日	|
+			values.put("week", week);
+			values.put("timeid", timeid);
+			values.put("subjectid", subjectid);
+			values.put("typeid", typeid);
+			values.put("share", isShare);
+			values.put("creatorid", creatorid);
+			values.put("uptime", timestamp);
+
+			//すでに登録されていないか検索
+			sql ="SELECT week,timeid FROM time WHERE week = " + week + " AND timeid =" + timeid +";";
+			db = sqlHelper.getReadableDatabase();
+			c = db.rawQuery(sql,null);
+			try {
+				if (c.getCount() != 0) {
+					sqlHelper.update("time", values,"week = ? AND timeid = ?",null);
+					Toast.makeText(this,"予定を更新しました",Toast.LENGTH_LONG).show();
+				} else {
+					Log.d(TAG, "data insert");
+					sqlHelper.insert("time", values);
+					Toast.makeText(this,"予定を登録しました",Toast.LENGTH_LONG).show();
+				}
+			} catch (SQLiteException e) {
+				e.printStackTrace();
+				Toast.makeText(this, "データの保存に失敗しました。", Toast.LENGTH_LONG).show();
+				return;
+			}
+
+			//		-----------------------------------------------------------------------
+			//
+			//		データを送信する
+			//
+			//		-----------------------------------------------------------------------
+			if (shareCb.isChecked()) {// シェアする歳の処理
+				Log.d(TAG, "データを送信する");
+				SnsSender sender = new SnsSender();
+				try {
+					sender.send(info);
+				} catch (SendException e) {
+					e.printStackTrace();
+					Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+				}
+			}
+
+			setResult(RESULT_OK);
+			finish();
+			sqlHelper.close();
 			db.close();
 		}
-		Log.d(TAG,"備考取得");
-		//		-----------------------------------------------------------------------
-		//
-		//		===================timeデータベースにデータを保存===========================
-		//
-		//		-----------------------------------------------------------------------
-
-		//time  |曜日	|時限ID	|授業科目ID	|種類ID	|シェア（時間割)	|作成者ID		|更新日	|
-		values.put("week", week);
-		values.put("timeid", timeid);
-		values.put("subjectid", subjectid);
-		values.put("typeid", typeid);
-		values.put("share", isShare);
-		values.put("creatorid", creatorid);
-		values.put("uptime", timestamp);
-
-		//すでに登録されていないか検索
-		sql ="SELECT week,timeid FROM time WHERE week = " + week + " AND timeid =" + timeid +";";
-		db = sqlHelper.getReadableDatabase();
-		c = db.rawQuery(sql,null);
-		try {
-			if (c.getCount() != 0) {
-				sqlHelper.update("time", values,"week = ? AND timeid = ?",null);
-				Toast.makeText(this,"予定を更新しました",Toast.LENGTH_LONG).show();
-			} else {
-				Log.d(TAG, "data insert");
-				sqlHelper.insert("time", values);
-				Toast.makeText(this,"予定を登録しました",Toast.LENGTH_LONG).show();
-			}
-		} catch (SQLiteException e) {
-			e.printStackTrace();
-			Toast.makeText(this, "データの保存に失敗しました。", Toast.LENGTH_LONG);
-			return;
-		}
-
-		//		-----------------------------------------------------------------------
-		//
-		//		データを送信する
-		//
-		//		-----------------------------------------------------------------------
-		if (shareCb.isChecked()) {// シェアする歳の処理
-			Log.d(TAG, "データを送信する");
-			SnsSender sender = new SnsSender();
-			try {
-				sender.send(info);
-			} catch (SendException e) {
-				e.printStackTrace();
-				Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG);
-			}
-		}
-
-		setResult(RESULT_OK);
-		finish();
-		sqlHelper.close();
-		db.close();
 	}
 
 	@Override
@@ -368,6 +373,7 @@ public class TimeTableEditActivity extends Activity implements OnClickListener {
 		default:
 			break;
 		}
+
 	}
 
 	// バック長押し用強制終了処理
